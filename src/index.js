6d24d23e18359d7c174a6a16363138745a3e6c3b
Laptop User <LaptopUser@JCPL-EXLAP-101.argo.uwf.edu>
2024-10-19 00:50:49 -0500

    Project added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./styles.css";
+import ColoredRect from "./App.js";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class FadeInAndOut extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = { show: false };
+
+    setInterval(() => {
+      this.setState({ show: !this.state.show });
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <div>
+        <a
+          onClick={() => {
+            this.setState({ show: !this.state.show });
+          }}
+          style={{ cursor: "pointer", padding: "20px" }}
+        >
+          Woah
+        </a>
+        <CSSTransition in={this.state.show} timeout={1200} classNames="base">
+          <div>Hello World</div>
+        </CSSTransition>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<ColoredRect />, document.getElementById("root"));
